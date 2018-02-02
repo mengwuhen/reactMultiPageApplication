b8329d6ca467eeb6bb1e0038ede0ab878dc072ae
[jie.shi@tarsocial.com] <[jie.shi@tarsocial.com]>
2018-02-02 14:15:58 +0800

    修改 redux的bug

--- app/component/pages/redux/index.js
+++ app/component/pages/redux/index.js
@@ -1,23 +1,49 @@
+// import React from 'react'
+// import ReactDOM from 'react-dom'
+// import { createStore } from 'redux'
+// import Counter from './components/Counter'
+// import counter from './reducer'
+
+// let store = createStore(counter)
+
+// console.warn(store)
+// console.warn(store.getState())
+// export default class Index extends React.Component{
+//     render(){
+//         return (
+//             <Counter
+//                 value={store.getState()}
+//                 onIncrement={() => store.dispatch({ type: 'INCREMENT' })}
+//                 onDecrement={() => store.dispatch({ type: 'DECREMENT' })}
+//             />
+//         )
+//     }
+// }
+
+// store.subscribe(Index)
+
 import React from 'react'
-import ReactDOM from 'react-dom'
-import { createStore } from 'redux'
+import {Provider, connect} from 'react-redux'
 import Counter from './components/Counter'
-import counter from './reducer'
+import store from './reducer/store'
 
-let store = createStore(counter)
-
-console.warn(store)
-console.warn(store.getState())
-export default class Index extends React.Component{
-    render(){
+class Main extends React.Component {
+    render() {
+        let state = this.props.storeState;
+        console.log(state);
         return (
             <Counter
-                value={store.getState()}
-                onIncrement={() => store.dispatch({ type: 'INCREMENT' })}
-                onDecrement={() => store.dispatch({ type: 'DECREMENT' })}
+                value={state.state}
+                onIncrement={() => store.dispatch({type: 'INCREMENT'})}
+                onDecrement={() => store.dispatch({type: 'DECREMENT'})}
             />
         )
     }
 }
 
-// store.subscribe(Index)
+const App = connect((state) => ({storeState: state}))(Main);
+export default () => (
+    <Provider store={store}>
+        <App/>
+    </Provider>
+);
--- app/component/pages/redux/reducer/index.js
+++ app/component/pages/redux/reducer/index.js
@@ -1,11 +1,24 @@
-export default (state = 0, action) => {
-  console.warn(action.type)
-    switch (action.type) {
-      case 'INCREMENT':
-        return state + 1
-      case 'DECREMENT':
-        return state - 1
-      default:
-        return state
+// export default (state = 0, action) => {
+//   console.warn(action.type)
+//     switch (action.type) {
+//       case 'INCREMENT':
+//         return state + 1
+//       case 'DECREMENT':
+//         return state - 1
+//       default:
+//         return state
+//     }
+//   }
+
+  export default (state = {state:0}, action) => {
+    console.warn(action.type)
+      switch (action.type) {
+        case 'INCREMENT':
+            console.log(1);
+            return Object.assign({}, state, {state: state.state + 1});
+        case 'DECREMENT':
+          return Object.assign({}, state, {state: state.state - 1});
+        default:
+          return state
+      }
     }
-  }
